cf85850299b26378a4a272c640050af761893240
Higor Araújo dos Anjos <higor.araujo.anjos@gmail.com>
2018-01-17 21:08:58 -0300

    Dados no gráfico de barras e novo gráfico de pizza
    +Adicionando os dados no gráfico de barras com o resultado da requisição do axios
    +Adicionando um gráfico novo de horas separado por segmentos
    +Criado um array de cores para estilizar o gráfico
    +Criado um json que simula a requisição dos dados de horas por segmento

--- src/components/ExecutedHours.js
+++ src/components/ExecutedHours.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Grid, Cell, Card, CardText, CardTitle, Divider } from 'react-md';
-import {Bar} from 'react-chartjs-2';
+import { Bar, Pie } from 'react-chartjs-2';
 import axios from 'axios';
 
 import constants from '../constants';
@@ -9,16 +9,17 @@ import constants from '../constants';
 export class ExecutedHours extends Component {
     constructor() {
         super(...arguments);
-        this.state={
+        this.state = {
             id_emissor: null,
-            data: []
+            data: [],
+            segmentos: []
         };
     }
-    
+
     componentWillReceiveProps(nextProps) {
         const prevstate = this.state;
         let id_emissor = nextProps.match.params.id_emissor;
-        if(id_emissor) {
+        if (id_emissor) {
             this.setState({ id_emissor: id_emissor });
             axios.get(`${constants.API_BASE_URL}/horas-executadas.json`)
                 .then(res => {
@@ -32,35 +33,72 @@ export class ExecutedHours extends Component {
                     this.setState(prevstate);
                     console.error('Error:', e);
                 });
+
+            axios.get(`${constants.API_BASE_URL}/horas-por-segmento.json`)
+                .then(res => {
+                    if (res.statusText === 'OK') {
+                        this.setState({ segmentos: res.data });
+                    } else {
+                        throw new Error('Server error!');
+                    }
+                })
+                .catch(e => {
+                    this.setState(prevstate);
+                    console.error('Error:', e);
+                });
         }
     }
-    
+
     render() {
-        const data = {
-            labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho'],
-            datasets: [
-              {
-                label: 'My First dataset',
-                backgroundColor: 'rgba(255,99,132,0.2)',
-                borderColor: 'rgba(255,99,132,1)',
-                borderWidth: 1,
-                hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-                hoverBorderColor: 'rgba(255,99,132,1)',
-                data: [65, 59, 80, 81, 56, 55, 40]
-              }
-            ]
-        };
+
+        const { data, segmentos } = this.state;
 
         return (
             <div>
                 <Grid>
                     <Cell desktopSize={8} desktopOffset={2}>
                         <Card>
-                            <CardTitle title="Horas executadas"/>
-                            <Divider/>
+                            <CardTitle title="Mensal" />
+                            <Divider />
                             <CardText>
                                 <Bar
-
+                                    data={{
+                                        labels: data.map(x => x.mes),
+                                        datasets: [
+                                            {
+                                                label: 'Desenvolvimento',
+                                                backgroundColor: 'rgba(255,99,132,0.2)',
+                                                borderColor: 'rgba(255,99,132,1)',
+                                                borderWidth: 1,
+                                                hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+                                                hoverBorderColor: 'rgba(255,99,132,1)',
+                                                data: data.map(x => x.horas)
+                                            }
+                                        ]
+                                    }}
+                                />
+                            </CardText>
+                        </Card>
+                    </Cell>
+                    <Cell desktopSize={8} desktopOffset={2}>
+                        <Card>
+                            <CardTitle title="Por categoria" />
+                            <Divider />
+                            <CardText>
+                                <Pie
+                                    data={{
+                                        labels: segmentos.map(x => x.segmento),
+                                        datasets: [
+                                            {
+                                                backgroundColor: constants.COLORS,
+                                                borderColor: 'rgba(255,255,255,1)',
+                                                borderWidth: 2,
+                                                hoverBackgroundColor: constants.COLORS,
+                                                hoverBorderColor: constants.COLORS,
+                                                data: segmentos.map(x => x.horas)
+                                            }
+                                        ]
+                                    }}
                                 />
                             </CardText>
                         </Card>
--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,4 @@
 export default {
-    API_BASE_URL: 'http://localhost:3000/'
+    API_BASE_URL: 'http://localhost:3000/',
+    COLORS : ['#F44336','#2196F3','#4CAF50','#FFEB3B','#FF9800','#E91E63','#795548','#673AB7','#607D8B','#9C27B0']
 };
